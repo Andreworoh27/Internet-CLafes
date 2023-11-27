55bb0b271ce46b05cc40ecd3cb5eaf4fe21b8903
elyssadg <elyssa.davina@gmail.com>
2023-11-27 23:28:33 +0700

    feats: edit job feature

--- src/controller/JobController.java
+++ src/controller/JobController.java
@@ -3,27 +3,42 @@ package controller;
 import java.util.List;
 
 import models.Job;
+import models.PC;
 
 public class JobController {
+	
+	private Job j = new Job();
 	
 	public void addNewJob(Integer userId, Integer pcId) {
 		
 	}
 	
-	public void updateJobStatus(Integer jobId, String jobStatus) {
+	public String updateJobStatus(Integer jobId, String jobStatus) {
+		if (!jobStatus.equals("Complete") && !jobStatus.equals("UnComplete"))
+			return "Job status must be either complete or uncomplete";
 		
+		j.updateJobStatus(jobId, jobStatus);
+		
+		Job job = j.getJobById(jobId);
+		PC pc = new PC().getPcDetail(job.getPcId());
+		if (jobStatus.equals("Complete"))
+			pc.updatePCCondition(job.getPcId(), "Usable");
+		else if (jobStatus.equals("UnComplete"))
+			pc.updatePCCondition(job.getPcId(), "Maintenance");
+		
+		return "Successfully update job status";
 	}
 	
 	public void getPcOnWorkingList(Integer pcId) {
-		
+		j.getPcOnWorkingList(pcId);
 	}
 	
-	public Job getTechnicianJob(Integer userId) {
-		return null;
+	public List<Job> getTechnicianJob(Integer userId) {
+		return j.getTechnicianJob(userId);
 	}
 	
 	public List<Job> getAllJobData() {
-		return null;
+		return j.getAllJobData();
 	}
 
 }
--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -9,19 +9,17 @@ public class ReportController {
 	
 	private Report r = new Report();
 	
-	public String addNewReport(String userRole, Integer pcId, String reportNote) {
+	public String addNewReport(String userRole, String pcId, String reportNote) {
 		PCController pcc = new PCController();
-		String errorMessage = "";
 		if (!userRole.equals("Customer") && !userRole.equals("Operator")) 
-			errorMessage = "User role must be either customer or operator";
-		else if (pcc.getPcDetail(errorMessage) == null)
-			errorMessage = "PC must be chosen and exist in database";
+			return "User role must be either customer or operator";
+		else if (pcc.getPcDetail(pcId) == null)
+			return "PC must be chosen and exist in database";
 		else if (reportNote.isEmpty())
-			errorMessage = "Report note must be filled";
+			return "Report note must be filled";
 		
-		if (errorMessage.isEmpty()) r.addNewReport(userRole, errorMessage, reportNote, new Date(System.currentTimeMillis()));
-		
-		return errorMessage;
+		r.addNewReport(userRole, pcId, reportNote, new Date(System.currentTimeMillis()));
+		return "Successfully added a new report";
 	}
 	
 	public List<Report> getAllReportData() {
