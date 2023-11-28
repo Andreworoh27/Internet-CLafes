01915f7531d2b63ed8224deef756432460ef96cd
guidowilliam11 <guido.william@binus.ac.id>
2023-11-28 22:43:24 +0700

    feats: BookPCForm Done

--- src/component/Card.java
+++ src/component/Card.java
@@ -14,6 +14,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import models.PC;
+import view.BookPCFormView;
 import view.PCView;
 import view.Page;
 import view.ReportPCFormView;
@@ -78,8 +79,8 @@ public class Card {
 
 	private void addAction(PC pc, Button book, Button report, Button update, Button delete) {
 		book.setOnAction(e -> {
-			pbc.getPCBookedData(pc.getPcId(), new Date(Calendar.getInstance().getTimeInMillis()));
-
+			BookPCFormView bookPCFormView = new BookPCFormView(pc, pcView);
+			layout.setRight(bookPCFormView.getContent());
 		});
 
 		report.setOnAction(e -> {
